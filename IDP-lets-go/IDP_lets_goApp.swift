1c4378dfc660baf8b911e7bb38cfc59d3a81e9c9
Laura Corssac <lauracorssac@gmail.com>
2024-12-01 22:22:24 +0100

    Initial Commit

---
+++ IDP-lets-go/IDP_lets_goApp.swift
@@ -0,0 +1,17 @@
+//
+//  IDP_lets_goApp.swift
+//  IDP-lets-go
+//
+//  Created by Laura Corssac on 01.12.24.
+//
+
+import SwiftUI
+
+@main
+struct IDP_lets_goApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
