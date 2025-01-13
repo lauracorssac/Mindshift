ea30f4c828ec32ef47f660be6a1fcdf583152c17
UmayBengisuBozkurt <bozkurtumaybengisu@gmail.com>
2025-01-13 14:36:36 +0100

    Add more details about categories and questions about the test.

--- IDP-lets-go/Test/TestQuestionView.swift
+++ IDP-lets-go/Test/TestQuestionView.swift
@@ -6,33 +6,64 @@
 //
 
 import SwiftUI
+
 struct TestQuestionView: View {
-    let category1: String
-    let category2: String
-    let question: String
+    let categories: [String] = ["Family", "Career", "Male", "Female", "Male or Family", "Female or Career", "Male or Career", "Female or Family"]
+    let maleQuestions: [String] = ["Ben", "Paul", "Daniel", "John", "Jeffrey"]
+    let femaleQuestions: [String] = ["Rebecca", "Michelle", "Emily", "Julia", "Anna"]
+    let careerQuestions: [String] = [ "Career", "Corporation", "Salary", "Office", "Professional", "Management", "Business"]
+    let familyQuestions: [String] = ["Wedding", "Marriage", "Parents", "Relatives", "Family", "Home", "Children"]
+    let selectedCategory1: String
+    let selectedCategory2: String
+    
+    let partOneCategories: [String] = ["Family", "Career"]
+    let partTwoCategories: [String] = ["Male", "Female"]
+    let partThreeCategories: [String] = ["Male or Family", "Female or Career"]
+    let partFourCategories: [String] = ["Male or Family", "Female or Career"]
+    let partFiveCategories: [String] = ["Male or Career", "Female or Career"]
+    let partSixCategories: [String] = ["Male or Career", "Female or Career"]
+    let partSevenCategories: [String] = ["Male or Career", "Female or Career"]
     
     var body: some View {
         VStack {
-            Text(question)
+            Text(maleQuestions[0])
                 .font(.title)
                 .padding( .bottom, 75)
             HStack {
-                Button(category1) {
+                Button(selectedCategory1) {
                     
                 }
                 .buttonStyle(RoundedButtonStyle())
                 .padding(.trailing, 15)
-                Button(category2) {
+                Button(selectedCategory2) {
                 }
                 .buttonStyle(RoundedButtonStyle())
                 }
                 
         }.padding()
     }
+    private func questions(for category: String) -> [String] {
+            var result = [String]()
+            
+            if category.contains("Male") {
+                result.append(contentsOf: maleQuestions)
+            }
+            if category.contains("Female") {
+                result.append(contentsOf: femaleQuestions)
+            }
+            if category.contains("Career") {
+                result.append(contentsOf: careerQuestions)
+            }
+            if category.contains("Family") {
+                result.append(contentsOf: familyQuestions)
+            }
+            
+            return result
+        }
 }
 
 #Preview {
-    TestQuestionView(category1: "Family", category2: "Career", question: "Salary")
+    TestQuestionView(selectedCategory1: "Family", selectedCategory2: "Career")
         .environmentObject(AppCoordinator())
 }
 
