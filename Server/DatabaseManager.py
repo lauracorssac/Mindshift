5372a7636797c5de149144d9585475a62b925b02
laura.corssac <lauracorssac@gmail.com>
2025-01-17 21:13:28 +0100

    adding some model separation to the server

---
+++ Server/DatabaseManager.py
@@ -0,0 +1,48 @@
+import sqlite3
+from models import User
+
+class DatabaseManager():
+
+    def init_db(self):
+        conn = sqlite3.connect('users.db')
+        cursor = conn.cursor()
+        cursor.execute('''
+            DROP TABLE IF EXISTS users
+        ''')
+        cursor.execute('''
+            CREATE TABLE users (
+                id TEXT PRIMARY KEY,
+                gender TEXT NOT NULL,
+                education TEXT NOT NULL,
+                race TEXT NOT NULL,
+                profession TEXT NOT NULL,
+                birthdate TEXT NOT NULL
+            )
+        ''')
+        conn.commit()
+        conn.close()
+
+    def save_user(self, user):
+        conn = sqlite3.connect('users.db')
+        cursor = conn.cursor()
+        cursor.execute('INSERT INTO users (id, gender, education, race, profession, birthdate) VALUES (?, ?, ?, ?, ?, ?)', 
+                       (str(user.id), user.gender.value, user.education.value, user.race, user.profession, user.birthdate))
+        conn.commit()
+        conn.close()
+
+    def get_all_users(self) -> list[User]:
+        conn = sqlite3.connect('users.db')
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM users')
+        rows = cursor.fetchall()
+        conn.close()
+
+        # Convert the result to a list of dictionaries
+        return [{
+            "id": row[0], 
+            "gender": row[1],
+            "education": row[2],
+            "birthdate": row[3],
+            "race": row[4],
+            "profession": row[5],
+            } for row in rows]
--- Server/server.py
+++ Server/server.py
@@ -1,50 +1,12 @@
-import sqlite3
+
 from flask import Flask, request, jsonify
-from pydantic import BaseModel, ValidationError
-from enum import Enum
-import uuid
+from pydantic import ValidationError
+from models import User
+from DatabaseManager import DatabaseManager
 
 app = Flask(__name__)
 
-class EducationBackground(str, Enum):
-    BACHELOR = "bachelor"
-    MASTER = "master"
-    DOCTORATE = "doctorate"
-    NONE = "none"
-    HIGH_SCHOOL = "highschool"
-
-class Gender(str, Enum):
-    FEMALE = "female"
-    MALE = "male"
-    NONE = "none"
-    OTHER = "other"
-
-class User(BaseModel):
-    id: uuid.UUID
-    gender: Gender = Gender.NONE
-    education: EducationBackground | None = None
-    birthdate: str = ""
-    race: str = ""
-    profession: str = ""
-
-def init_db():
-    conn = sqlite3.connect('users.db')
-    cursor = conn.cursor()
-    cursor.execute('''
-        DROP TABLE IF EXISTS users
-    ''')
-    cursor.execute('''
-        CREATE TABLE users (
-            id TEXT PRIMARY KEY,
-            gender TEXT NOT NULL,
-            education TEXT NOT NULL,
-            race TEXT NOT NULL,
-            profession TEXT NOT NULL,
-            birthdate TEXT NOT NULL
-        )
-    ''')
-    conn.commit()
-    conn.close()
+database_manager = DatabaseManager()
 
 @app.route('/user', methods=['POST'])
 def receive_user():
@@ -54,12 +16,7 @@ def receive_user():
 
         print(str(user.id), user.gender.value, user.education.value, user.race, user.profession, user.birthdate)
 
-        conn = sqlite3.connect('users.db')
-        cursor = conn.cursor()
-        cursor.execute('INSERT INTO users (id, gender, education, race, profession, birthdate) VALUES (?, ?, ?, ?, ?, ?)', 
-                       (str(user.id), user.gender.value, user.education.value, user.race, user.profession, user.birthdate))
-        conn.commit()
-        conn.close()
+        database_manager.save_user(user)
 
         response = {"message": "OK"}
         return jsonify(response), 200
@@ -75,21 +32,7 @@ def receive_user():
 @app.route('/users', methods=['GET'])
 def get_all_users():
     try:
-        conn = sqlite3.connect('users.db')
-        cursor = conn.cursor()
-        cursor.execute('SELECT * FROM users')
-        rows = cursor.fetchall()
-        conn.close()
-
-        # Convert the result to a list of dictionaries
-        users = [{
-            "id": row[0], 
-            "gender": row[1],
-            "education": row[2],
-            "birthdate": row[3],
-            "race": row[4],
-            "profession": row[5],
-            } for row in rows]
+        users = database_manager.get_all_users()
 
         # Respond with the list of users
         return jsonify({"users": users}), 200
@@ -98,5 +41,5 @@ def get_all_users():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    init_db()
+    database_manager.init_db()
     app.run(host='0.0.0.0', port=4001)
