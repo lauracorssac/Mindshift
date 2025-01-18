d7c211dcedaa1fb6f09ebb10233ec6efeacb09fa
UmayBengisuBozkurt <bozkurtumaybengisu@gmail.com>
2025-01-18 19:50:04 +0300

    Remove to do statement.

--- IDP-lets-go/Consent/Consent.swift
+++ IDP-lets-go/Consent/Consent.swift
@@ -18,7 +18,6 @@ Lorem ipsum dolor sit amet, adipiscing elit. Nulla quam velit, vulputate eu phar
 
 class Consent {
     
-    // TODO: Change to real text
     let text =
 """
 The study is for scientific purposes only, and your answers will not be shared with third parties. Your answers will only be evaluated in aggregated form and completely anonymously by our research team. All information will be kept strictly confidential, and all data will be processed and stored in accordance with the ethical guidelines of the German Research Foundation. Participation is completely voluntary. If you feel uncomfortable answering any questions, you may skip that question or quit the survey at any time. Of course, all data will be treated confidentially and EU-DSGVO compliant.
