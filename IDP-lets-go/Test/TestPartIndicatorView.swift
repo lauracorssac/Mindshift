714232fc94baa9f841eeea2c66ac55ef63d640b2
UmayBengisuBozkurt <bozkurtumaybengisu@gmail.com>
2025-01-18 17:13:52 +0300

    Add sdditional text for part 4 and 6.

--- IDP-lets-go/Test/TestPartIndicatorView.swift
+++ IDP-lets-go/Test/TestPartIndicatorView.swift
@@ -40,8 +40,11 @@ struct TestPartIndicatorView: View {
                             .font(.headline)
                             .padding(.horizontal)
                             .multilineTextAlignment(.center)
-          
+            
             Text("""
+                \(step.stepNumber == 4 || step.stepNumber == 6
+                    ? "This part is the same as the previous part. "
+                    : "")\
                 Click left button for \(step.leftTitle). Click right button for \(step.rightTitle). Items will appear one at a time. If you make a mistake, click the right answer to continue. Go fast as you can while being accurate.
                 """)
                 .multilineTextAlignment(.center)
