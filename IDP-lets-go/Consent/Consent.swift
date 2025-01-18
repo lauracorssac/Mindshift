16fdcec93d8bc4daeef17bb109238839afab0104
UmayBengisuBozkurt <bozkurtumaybengisu@gmail.com>
2025-01-18 19:49:21 +0300

    Add consent text.

--- IDP-lets-go/Consent/Consent.swift
+++ IDP-lets-go/Consent/Consent.swift
@@ -21,7 +21,13 @@ class Consent {
     // TODO: Change to real text
     let text =
 """
-Lorem ipsum dolor sit amet, adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac nibh. Lorem ipsum dolor sit amet, adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac nibh. Lorem ipsum dolor sit amet, adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac nibh.
+The study is for scientific purposes only, and your answers will not be shared with third parties. Your answers will only be evaluated in aggregated form and completely anonymously by our research team. All information will be kept strictly confidential, and all data will be processed and stored in accordance with the ethical guidelines of the German Research Foundation. Participation is completely voluntary. If you feel uncomfortable answering any questions, you may skip that question or quit the survey at any time. Of course, all data will be treated confidentially and EU-DSGVO compliant.
+
+If you have any questions or comments about this study, please contact Nadja Born at nadja.born@tum.de.
+ 
+By clicking on the next button, you agree to the following: I have read and understood the above information. I hereby declare that I am at least 18 years old and consent to the use of my data for internal evaluation at the Technical University of Munich. I am aware that my participation is voluntary and that I can cancel the survey at any time without giving reasons. 
+ 
+Do you consent to participate in this survey?
 """
     
 }
